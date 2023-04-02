4d77640688eae1c0740c370a445503ff75b04e01
Brent Houston <brentjustinhouston@gmail.com>
2023-04-02 01:45:36 -0700

    count added

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
 // define all questions and answers
-var questions = ["How much does a full NASA space suit cost?", "How many earth's could fit inside the Sun?", "If you were watching a sunset on Mars what color would it be?", "The Sun's mass takes up what percentage of our solar system?", "Very light snow is known to occur at high latitudes on which planet?", "What is the name of the black hole thought to exist at the center of the Milky Way?", "In our solor system, how many stars are there?", "What is the maximum temperature theat Venus' surface can reach in degrees Fahrenheit?", "How many stars are there in the Milky Way galaxy?"]
+var questions = ["How much does a full NASA space suit cost?", 
+"How many earth's could fit inside the Sun?", 
+"If you were watching a sunset on Mars what color would it be?", 
+"The Sun's mass takes up what percentage of our solar system?", 
+"Very light snow is known to occur at high latitudes on which planet?", 
+"What is the name of the black hole thought to exist at the center of the Milky Way?", 
+"In our solor system, how many stars are there?", 
+"What is the maximum temperature that Venus' surface can reach in degrees Fahrenheit?", 
+"How many stars are there in the Milky Way galaxy?"]
 var answers = ["$ 12,000,000", "1 million", "blue", "99.86%", "Mars", "Sagittarius A*", "1", "850", "100 thousand million"]
 // define variable for tracking
 //      track time
@@ -7,46 +15,57 @@ var countdownTimer;
 var secondsLeft = 100;
 var isPlaying = false;
 var randomQuestion = "";
-//      track questions
+var scoreCard = "";
+// var count = localStorage.getItem("count")
+var count = 0;
 
+//      track questions
+var countH2 = document.querySelector("#uscore")
 // create variables to reference DOM elements
 //      timer
 var timeH2 = document.querySelector("#timer")
 //      questions
 var questionsH3 = document.querySelector("#ask")
 //      answers
-var answersP
+var answersDiv = document.querySelector("#answer")
 //      start button
 var startButton = document.querySelector("#start-game")
+//      score
+var scoreCard = document.querySelector("#count")
+var wrongH2 = document.querySelector("#wrong")
 var btn1 = document.querySelector("#btn1");
 var btn2 = document.querySelector("#btn2");
 var btn3 = document.querySelector("#btn3");
 var btn4 = document.querySelector("#btn4");
+var truthy = document.querySelector("button").querySelector(".class")
 
+var p = document.querySelector("button");
 
+scoreCard.textContent = count;
 
-
-
-document.querySelector("#start-game").addEventListener("click",function(){
-if(!isPlaying){
-    
-    startQuiz();
-}
-})
 //      name/initials
 //      save button
+
+document.querySelector("#start-game").addEventListener("click",function(){
+    if(!isPlaying){
+    
+    startQuiz();
+    }
+});
+
+
 function showHide() {
     document.querySelector(".hide").setAttribute("name", ".show")
     if(document.querySelector)startButton.remove();
-    
-
-
 }
+//      high scores container
 //      high scores container
 
 // function
 //      start quiz
- function startQuiz (){
+function startQuiz (){
+   
+    countH2.textContent=count;
     secondsLeft =100;
     timeH2.textContent=secondsLeft;
     isPlaying=true;
@@ -55,93 +74,125 @@ function showHide() {
      //          show the quiz container
 
      //          display first question
-     randomQuestion = questions[Math.floor(Math.random()* questions.length)];
-     console.log("randomQuestion:",randomQuestion);
-     questionsH3.textContent=randomQuestion;
-     if(randomQuestion == questions[0]){
+     askQuestion();
+     
+        //          start timer
+        countdownTimer = setInterval(function(){
+            secondsLeft--;
+            timeH2.textContent=secondsLeft;
+            if(secondsLeft<=0){
+                clearInterval(countdownTimer);
+                timeH2.textContent ="time up!"
+                isPlaying=false;
+                
+                
+            }
+        },1000)
+        //          display countdown on screen
+        
+    }
+    //          display countdown on screen
+    
+    
+    // function
+    //      rendering a question
+function askQuestion() {
+    wrongH2.textContent="";
+    btn1.setAttribute("class", "false");
+    btn2.setAttribute("class", "false");
+    btn3.setAttribute("class", "false");
+    btn4.setAttribute("class", "false");
+    randomQuestion = questions[Math.floor(Math.random()* questions.length)];
+    console.log("randomQuestion:",randomQuestion);
+    questionsH3.textContent=randomQuestion;
+    if(randomQuestion == questions[0]){
+        btn3.setAttribute("class", "true");
         btn1.textContent="$ 500,000"
         btn2.textContent="$ 3,000,000"
         btn3.textContent="$ 12,000,000"
         btn4.textContent="$ 40,000,000"
-        }else if(randomQuestion == questions[1]){
+    }else if(randomQuestion == questions[1]){
+        btn1.setAttribute("class", "true");
         btn1.textContent="1 million"
         btn2.textContent="1 trillion"
         btn3.textContent="100 thousand"
         btn4.textContent="500 thousand"
-        }else if(randomQuestion == questions[2]){
+    }else if(randomQuestion == questions[2]){
+        btn4.setAttribute("class", "true");
         btn1.textContent="Green"
         btn2.textContent="Orange"
         btn3.textContent="Yellow"
         btn4.textContent="Blue"
-        }else if(randomQuestion == questions[3]){
+    }else if(randomQuestion == questions[3]){
+        btn4.setAttribute("class", "true");
         btn1.textContent="33.33%"
         btn2.textContent="50.61%"
         btn3.textContent="82.66%"
         btn4.textContent="99.86%"
-        }else if(randomQuestion == questions[3]){
+    }else if(randomQuestion == questions[4]){
+        btn2.setAttribute("class", "true");
         btn1.textContent="Neptune"
         btn2.textContent="Mars"
         btn3.textContent="Venus"
         btn4.textContent="Saturn"
-        }else if(randomQuestion == questions[4]){
+    }else if(randomQuestion == questions[5]){
+        btn3.setAttribute("class", "true");
         btn1.textContent="Capricorn C*"
         btn2.textContent="Gemini B*"
         btn3.textContent="Sagittarius A*"
         btn4.textContent="Scorpio D*"
-        }else if(randomQuestion == questions[5]){
+    }else if(randomQuestion == questions[6]){
+        btn4.setAttribute("class", "true");
         btn1.textContent="100 million"
         btn2.textContent="30 trillion"
         btn3.textContent="50 million"
         btn4.textContent="1"
-        }else if(randomQuestion == questions[6]){
-        btn1.textContent="1 million"
-        btn2.textContent="1 trillion"
-        btn3.textContent="100 thousand"
-        btn4.textContent="500 thousand"
-        }else if(randomQuestion == questions[7]){
+    }else if(randomQuestion == questions[7]){
+        btn1.setAttribute("class", "true");
         btn1.textContent="850"
         btn2.textContent="1500"
         btn3.textContent="530"
         btn4.textContent="2000"
-        }else if(randomQuestion == questions[8]){
+    }else if(randomQuestion == questions[9]){
+        btn3.setAttribute("class", "true");
         btn1.textContent="500 trillion"
         btn2.textContent="65 trillion"
-        btn3.textContent="100 thousand million"
+        btn3.textContent="100 billion"
         btn4.textContent="500 thousand million"
-            
-
+        
         }
-     //          start timer
-     countdownTimer = setInterval(function(){
-        secondsLeft--;
-        timeH2.textContent=secondsLeft;
-        if(secondsLeft<=0){
-            clearInterval(countdownTimer);
-            timeH2.textContent ="time up!"
-            isPlaying=false;
+        
+    };
+    
+    checkTruthy();
 
 
-        }
-     },1000)
-     //          display countdown on screen
-     
-    }
-// function
-//      rendering a question
 //          clear/remove previous question
-//          getting first question
-//          add question to the question container
-//          make button for each answer
-//          add answers to the answers container
+    //          getting first question
+    //          add question to the question container
+    //          make button for each answer
+    //          add answers to the answers container
+    
+    // function
+    //      handle quiz completion
+    //          stop timer
+    //          hide quiz container
+    //          show end screen
+    //          show time remaining as score
+    
+ function checkTruthy() {
+    if(truthy != true){
+        countdownTimer = countdownTimer - 100;
+        wrongH2.textContent= "Wrong"
+        askQuestion();
+    }else{
+        count++;
+        askQuestion();
+            
+     }
+     console.log("count:",count);
+ }
 
-// function
-//      handle quiz completion
-//          stop timer
-//          hide quiz container
-//          show end screen
-//          show time remaining as score
-
-// function
 //      handle answer clicks
 //
 //      if
@@ -183,5 +234,12 @@ function showHide() {
 // event listeners
 //      click start
 //      click answers
+
+
+btn1.addEventListener("click", checkTruthy);
+btn2.addEventListener("click", checkTruthy);
+btn3.addEventListener("click", checkTruthy);
+btn4.addEventListener("click", checkTruthy);
 //      click save score
-//      keyups
+
+    //  keyups
